aae5a6e2de9fed5f911b0e54f3285a724e487bb2
Andres Paez <andres@movinglake.com>
2024-07-22 13:26:33 -0600

    Remove htmx

--- handler/htmlhandler.go
+++ handler/htmlhandler.go
@@ -19,25 +19,29 @@ func NewHavenHTMLHandler(db wrappers.DB) *HavenHTMLHandler {
 	}
 }
 
+func (h *HavenHTMLHandler) home(c *gin.Context) {
+	resources, err := h.db.GetAllResources()
+	if err != nil {
+		log.Printf("Error getting resources: %v", err)
+	}
+	var formattedResources []string
+	for _, r := range resources {
+		formattedResources = append(formattedResources, r.Name)
+	}
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"title":     "Haven",
+		"resources": formattedResources,
+		"config":    "",
+		"logs":      "",
+		"metrics":   "",
+	})
+}
+
 func (h *HavenHTMLHandler) RegisterRoutes(r *gin.Engine, templateRegex string, staticDir string) {
 	r.LoadHTMLGlob(templateRegex)
-	r.GET("/index", func(c *gin.Context) {
-		resources, err := h.db.GetAllResources()
-		if err != nil {
-			log.Printf("Error getting resources: %v", err)
-		}
-		var formattedResources []string
-		for _, r := range resources {
-			formattedResources = append(formattedResources, r.Name)
-		}
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title":     "Haven",
-			"resources": formattedResources,
-			"config":    "",
-			"logs":      "",
-			"metrics":   "",
-		})
-	})
+	r.GET("/", h.home)
+	r.GET("/index", h.home)
+	r.GET("/index.html", h.home)
 	r.GET("/resource/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		c.HTML(http.StatusOK, "resource.html", gin.H{
